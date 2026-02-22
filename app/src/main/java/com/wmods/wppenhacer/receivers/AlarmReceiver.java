0186e10f4505870bbf10b76b5599db5d42a18156
RH71 <67680354+Kawansejalanproduction2019@users.noreply.github.com>
2026-02-22 15:43:04 +0700

    Update AlarmReceiver.java

--- app/src/main/java/com/wmods/wppenhacer/receivers/AlarmReceiver.java
+++ app/src/main/java/com/wmods/wppenhacer/receivers/AlarmReceiver.java
@@ -12,15 +12,17 @@ public class AlarmReceiver extends BroadcastReceiver {
             String msg = intent.getStringExtra("MESSAGE");
 
             if (jid != null && msg != null) {
+                // Tembak langsung ke WhatsApp Normal
                 Intent waIntent = new Intent("com.wmods.wppenhacer.EXECUTE_SCHEDULE");
-                waIntent.setPackage("com.whatsapp");
+                waIntent.setClassName("com.whatsapp", "com.whatsapp.appwidget.WidgetProvider");
                 waIntent.putExtra("JID", jid);
                 waIntent.putExtra("MESSAGE", msg);
                 waIntent.addFlags(Intent.FLAG_RECEIVER_FOREGROUND);
                 context.sendBroadcast(waIntent);
 
+                // Tembak langsung ke WhatsApp Business
                 Intent wbIntent = new Intent("com.wmods.wppenhacer.EXECUTE_SCHEDULE");
-                wbIntent.setPackage("com.whatsapp.w4b");
+                wbIntent.setClassName("com.whatsapp.w4b", "com.whatsapp.appwidget.WidgetProvider");
                 wbIntent.putExtra("JID", jid);
                 wbIntent.putExtra("MESSAGE", msg);
                 wbIntent.addFlags(Intent.FLAG_RECEIVER_FOREGROUND);
